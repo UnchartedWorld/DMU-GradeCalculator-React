81ed0fb79e0e15e2e728e07e2097c82c3e8e5c64
UnchartedWorld <nathangvjcollins@gmail.com>
2023-05-26 00:23:03 +0100

    feat(routes): Adds About and Calculator routes
    As stated above.

---
+++ src/routes/About.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mantine/core";
+
+
+export default function About() {
+    return (
+        <Container py={"md"} role="region">
+            
+        </Container>
+    )
+}
---
+++ src/routes/Calculator.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mantine/core";
+
+
+export default function Calculator() {
+    return (
+        <Container py={"md"} role="region">
+            
+        </Container>
+    )
+}
