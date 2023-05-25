951050b33f51d1f4f9a13ad7e270d71edcb242ec
UnchartedWorld <nathangvjcollins@gmail.com>
2023-05-26 00:20:05 +0100

    feat(app): Adds color scheme wrapper and routes
    This allows the dark mode toggle button to work, alongside the routes to, well, work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import "./App.css";
-import { MantineProvider, Text } from "@mantine/core";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import { Navbar } from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import { useColorScheme } from "@mantine/hooks";
+import { useState } from "react";
 
 function App() {
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] =
+    useState<ColorScheme>(preferredColorScheme);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text>Mantine test text</Text>
-    </MantineProvider>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider theme={{colorScheme}} withGlobalStyles withNormalizeCSS>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
 
