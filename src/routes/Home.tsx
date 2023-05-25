30c56a92b2d4949f1857161fc9aab190fc2a04e9
UnchartedWorld <nathangvjcollins@gmail.com>
2023-05-26 00:25:48 +0100

    feat(routes): Adds home page with some placeholder text
    As stated above, adds placeholder text. In short time, this will be updated and fixed.

---
+++ src/routes/Home.tsx
@@ -0,0 +1,26 @@
+import { Button, Container, Text, Title } from "@mantine/core";
+
+export default function Home() {
+  return (
+    <Container py={"md"} role="region">
+      <Title
+        id="homeTitle"
+        variant="gradient"
+        gradient={{ from: "grape", to: "violet", deg: 40 }}
+        ta={"center"}
+        order={1}
+        ff={"sans-serif"}
+      >
+        Calculate your grade!
+      </Title>
+      <Text sx={{ paddingTop: "1rem" }} ta={"center"}>
+        Did you start your course in 2020/2021? Use this calculator to discover
+        what your predicted grade is likely to be. If you're in another year
+        group, feel free to make some suggestions via an issue on how best to
+        implement alternate grading.
+      </Text>
+
+      <Button mt={"1rem"}>Get started</Button>
+    </Container>
+  );
+}
