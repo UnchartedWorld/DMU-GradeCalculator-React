a2afb3421ad3b892d16aff7a74fe5444466226be
UnchartedWorld <nathangvjcollins@gmail.com>
2023-07-29 15:24:55 +0100

    feat(app): Adds a FAB to return user to the top of the webpage
    This is an entirely minor feature, but may prove useful for some users.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,47 @@
 import "./App.css";
 import {
+  Affix,
+  Button,
   ColorScheme,
   ColorSchemeProvider,
   MantineProvider,
+  Transition,
+  rem,
 } from "@mantine/core";
 import { Navbar } from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
-import { useColorScheme } from "@mantine/hooks";
+import { useColorScheme, useWindowScroll } from "@mantine/hooks";
 import { useState } from "react";
 import Calculator from "./routes/Calculator";
+import { IconArrowUp } from "@tabler/icons-react";
 
 function App() {
   const preferredColorScheme = useColorScheme();
-  const [colorScheme, setColorScheme] =
-    useState<ColorScheme>(preferredColorScheme);
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme);
+  const [scroll, scrollTo] = useWindowScroll();
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider theme={{colorScheme}} withGlobalStyles withNormalizeCSS>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
         <Navbar />
+        <Affix position={{ bottom: rem(30), right: rem(30) }}>
+          <Transition transition={"pop"} mounted={scroll.y > 0}>
+            {(transitionStyles) => (
+              <Button
+                leftIcon={<IconArrowUp size={"1rem"} />}
+                style={transitionStyles}
+                radius={"lg"}
+                onClick={() => scrollTo({ y: 0 })}
+              >
+                Scroll up to top
+              </Button>
+            )}
+          </Transition>
+        </Affix>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
